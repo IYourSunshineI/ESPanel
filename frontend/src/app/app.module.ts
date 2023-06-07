a2b0a4cf4f5e7470b7c0583a5cc88834b75d9269
Sebastian Klaus <e12120487@student.tuwien.ac.at>
2023-06-07 19:36:43 +0200

    Implemented layout for objects

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { ModulesComponent } from './components/modules/modules.component';
+import { ObjectComponent } from './components/object/object.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    ModulesComponent,
+    ObjectComponent,
   ],
   imports: [
     BrowserModule
