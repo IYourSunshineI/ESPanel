86758d7807c464df9e4c93a02927c54d3cb3767b
Sebastian Klaus <e12120487@student.tuwien.ac.at>
2023-06-07 18:32:19 +0200

    Implemented layout for sidebar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ModulesComponent } from './components/modules/modules.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SidebarComponent,
+    ModulesComponent,
   ],
   imports: [
     BrowserModule
