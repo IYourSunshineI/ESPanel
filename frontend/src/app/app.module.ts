5f3c5a4e507653e21faa6c9ebfaff6e477837486
Sebastian Klaus <e12120487@student.tuwien.ac.at>
2023-06-08 11:58:34 +0200

    refactored layout of main page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { ObjectComponent } from './components/object/object.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent,
-    ObjectComponent,
   ],
   imports: [
     BrowserModule
