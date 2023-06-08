b4c7559ed6b44e780bc591effb3abef8edf18d8a
Sebastian Klaus <e12120487@student.tuwien.ac.at>
2023-06-08 12:54:42 +0200

    Implemented group component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GroupComponent } from './components/group/group.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    GroupComponent,
   ],
   imports: [
     BrowserModule
