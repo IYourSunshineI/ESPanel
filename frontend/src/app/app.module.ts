d5f0e90f6ab1887041beb4d1acd20b7b2696e80a
Sebastian Klaus <e12120487@student.tuwien.ac.at>
2023-06-08 13:58:00 +0200

    Implemented switch component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { GroupComponent } from './components/group/group.component';
+import { SwitchComponent } from './components/switch/switch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GroupComponent,
+    SwitchComponent,
   ],
   imports: [
     BrowserModule
