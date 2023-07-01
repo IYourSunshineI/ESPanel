c087829c6e42f221042b650d081b6134f7499f87
Sebastian Klaus <e12120487@student.tuwien.ac.at>
2023-07-01 11:45:03 +0200

    [#3] Added DataIntegrityViolation to exceptionHandler

--- backend/src/main/java/com/yoursunshine/backend/endpoint/GlobalExceptionHandler.java
+++ backend/src/main/java/com/yoursunshine/backend/endpoint/GlobalExceptionHandler.java
@@ -3,6 +3,9 @@ package com.yoursunshine.backend.endpoint;
 import com.yoursunshine.backend.exception.NotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -36,5 +39,17 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Object> handleNotFoundException(NotFoundException ex) {
         LOGGER.warn("handleNotFoundException: {}", ex.getMessage());
         return ResponseEntity.notFound().build();
+    }
+
+    /**
+     * Handle data integrity violation exceptions.
+     *
+     * @param ex the exception
+     * @return the response entity
+     */
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        LOGGER.warn("handleDataIntegrityViolationException: {}", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
     }
 }
