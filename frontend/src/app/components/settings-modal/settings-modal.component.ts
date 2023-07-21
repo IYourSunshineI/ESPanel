ec33778e43d6efe91f13be514c7e1c7136bede95
Sebastian Klaus <e12120487@student.tuwien.ac.at>
2023-07-21 20:19:46 +0200

    [#3] Implemented foldout for Group Settings

--- frontend/src/app/components/settings-modal/settings-modal.component.ts
+++ frontend/src/app/components/settings-modal/settings-modal.component.ts
@@ -1,7 +1,8 @@
 import {AfterContentInit, Component} from '@angular/core';
 import {Room} from "../../dtos/room";
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
-import {RoomService} from "../../services/room.service";
+import {Group} from "../../dtos/group";
+import {GroupService} from "../../services/group.service";
 
 @Component({
   selector: 'app-settings-modal',
@@ -13,9 +14,12 @@ export class SettingsModalComponent implements AfterContentInit {
   room: Room;
   updatedRoom: Room;
 
+  groups: Group[] = [];
+  showGroups: boolean = false;
+
   constructor(
     public activeModal: NgbActiveModal,
-    private service: RoomService,
+    private groupService: GroupService,
   ) {
   }
 
@@ -26,6 +30,20 @@ export class SettingsModalComponent implements AfterContentInit {
   updateRoom($event: Room) {
     this.room = $event;
     this.activeModal.dismiss('updated');
+  }
+
+  loadGroups() {
+    if(this.groups.length > 0) return;
+    if(!this.room.id) return;
+
+    this.groupService.getAll(this.room.id).subscribe({
+      next: data => {
+        this.groups = data;
+      },
+      error: e => {
+        console.error(e);
+      }
+    });
   }
 
 }
