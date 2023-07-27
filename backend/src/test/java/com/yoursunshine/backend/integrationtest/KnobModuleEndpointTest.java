cce59fe8d520c785434f4d369eaaeca15d3c712b
Sebastian Klaus <e12120487@student.tuwien.ac.at>
2023-07-27 15:32:44 +0200

    [#4] Implemented test for deletion of knobModules

---
+++ backend/src/test/java/com/yoursunshine/backend/integrationtest/KnobModuleEndpointTest.java
@@ -0,0 +1,59 @@
+package com.yoursunshine.backend.integrationtest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yoursunshine.backend.datagenerator.DataGeneratorBean;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.sql.SQLException;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles({"test", "datagen"})
+@SpringBootTest
+@EnableWebMvc
+@WebAppConfiguration
+public class KnobModuleEndpointTest {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private DataGeneratorBean generatorBean;
+
+    @BeforeEach
+    public void beforeEach() throws SQLException {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        generatorBean.generateData();
+    }
+
+    @AfterEach
+    public void afterEach() throws SQLException {
+        generatorBean.deleteData();
+    }
+
+    @Test
+    public void givenExistingModule_whenDelete_thenDeleteAndReturn204() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .delete("/rooms/-1/groups/-1/knobmodules/-1"))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void givenNonExistingModule_whenDelete_thenReturn404() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .delete("/rooms/-1/groups/-1/knobmodules/-99"))
+                .andExpect(status().isNotFound());
+    }
+}
---
+++ backend/src/test/java/com/yoursunshine/backend/unittest/KnobModuleServiceTest.java
@@ -0,0 +1,48 @@
+package com.yoursunshine.backend.unittest;
+
+import com.yoursunshine.backend.datagenerator.DataGeneratorBean;
+import com.yoursunshine.backend.exception.NotFoundException;
+import com.yoursunshine.backend.service.KnobModuleService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ActiveProfiles({"test", "datagen"})
+@SpringBootTest
+public class KnobModuleServiceTest {
+    @Autowired
+    private KnobModuleService knobModuleService;
+    @Autowired
+    private DataGeneratorBean generatorBean;
+
+    @BeforeEach
+    public void beforeEach() throws SQLException {
+        generatorBean.generateData();
+    }
+
+    @AfterEach
+    public void afterEach() throws SQLException {
+        generatorBean.deleteData();
+    }
+
+    @Test
+    public void givenExistingModule_whenDelete_thenDelete() {
+        int sizeBefore = knobModuleService.getAll().size();
+        knobModuleService.delete(-1L);
+        int sizeAfter = knobModuleService.getAll().size();
+        assertEquals(sizeBefore - 1, sizeAfter);
+    }
+
+    @Test
+    public void givenNonExistingModule_whenDelete_thenThrowNotFoundException() {
+        assertThrows(NotFoundException.class, () -> knobModuleService.delete(-100L));
+    }
+}
