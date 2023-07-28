b4c0c0a4c7f88b7727efbb152beb430e3a9a7945
Sebastian Klaus <e12120487@student.tuwien.ac.at>
2023-07-28 11:19:01 +0200

    [#4] Implemented initial value for rgb knob

--- frontend/src/app/components/module/module.component.ts
+++ frontend/src/app/components/module/module.component.ts
@@ -12,8 +12,28 @@ export class ModuleComponent {
   @Input() type: ModuleType;
   protected readonly ModuleType = ModuleType;
 
+  rgbValue: { R: number; B: number; G: number } | null = null;
+
 
   getInitialDimmerValue(): number {
     return (this.module as DimmerModule).brightness;
+  }
+
+  getInitialRgbValue(i: string): number {
+    if(!this.rgbValue) {
+       this.rgbValue = this.hexToRgb((this.module as RgbModule).color);
+    }
+    if(!this.rgbValue) return 0;
+    // @ts-ignore
+    return this.rgbValue[i];
+  }
+
+  private hexToRgb(hex: string): { R: number; B: number; G: number } | null {
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+      'R': parseInt(result[1], 16),
+      'G': parseInt(result[2], 16),
+      'B': parseInt(result[3], 16)
+    } : null;
   }
 }
